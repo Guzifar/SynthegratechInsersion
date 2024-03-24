c955f0718aa0cbebeb6556de4d64cdb4aae78518
Guzifar S. Bungkac <guzifarbungkac@gmail.com>
2024-03-25 01:51:14 +0800

    Updated

--- app/src/main/java/com/example/synthegratechinsertion/MainActivity.java
+++ app/src/main/java/com/example/synthegratechinsertion/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                                             bundle.putString("name", name);
                                             bundle.putString("email", email);
                                             bundle.putString("type", type);
-                                            Intent intent = new Intent(MainActivity.this, AddExpense.class);
+                                            Intent intent = new Intent(MainActivity.this, Dashboard.class);
                                             intent.putExtras(bundle);
                                             startActivity(intent);
                                         }

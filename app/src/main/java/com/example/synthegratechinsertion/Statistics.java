f3514963bd171143e5de058c9c52bca79eb0f2ad
Guzifar S. Bungkac <guzifarbungkac@gmail.com>
2024-04-01 04:59:32 +0800

    Updated: Added Statics Bar and Pie Chart

--- app/src/main/java/com/example/synthegratechinsertion/Statistics.java
+++ app/src/main/java/com/example/synthegratechinsertion/Statistics.java
@@ -7,6 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class Statistics extends AppCompatActivity {
     String name = "", email = "", type = "";
     @Override
@@ -19,6 +31,34 @@ public class Statistics extends AppCompatActivity {
             email = bundle.getString("email");
             type = bundle.getString("type");
         }
+        BarChart barChart = findViewById(R.id.barChart);
+        PieChart pieChart = findViewById(R.id.pieChart);
+
+        ArrayList<BarEntry> barDatas = new ArrayList<>();
+        ArrayList<PieEntry> pieDatas = new ArrayList<>();
+
+        for (int i=1; i<10; i++){
+            float value = (float) (i*10.0);
+            BarEntry barData = new BarEntry(i, value);
+            barDatas.add(barData);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barDatas, "Expenses");
+        barDataSet.setColors(ColorTemplate.PASTEL_COLORS);
+        barDataSet.setDrawValues(false);
+        barChart.setData(new BarData(barDataSet));
+
+        for (int i=1; i<10; i++){
+            float value = (float) (i*10.0);
+            PieEntry pieData = new PieEntry(i, value);
+            pieDatas.add(pieData);
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(pieDatas, "Expenses");
+        pieDataSet.setColors(ColorTemplate.PASTEL_COLORS);
+        pieDataSet.setDrawValues(false);
+        pieChart.setData(new PieData(pieDataSet));
+
         ImageButton aButton = findViewById(R.id.accountButton);
         ImageButton hButton = findViewById(R.id.homeButton);
         aButton.setOnClickListener(new View.OnClickListener() {
